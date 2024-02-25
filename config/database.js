2057c553f2bc49e7aef5d3b246c9bffbc6f62317
linguosity <brandon@linguosity.ai>
2024-02-25 09:16:55 -0800

    folder set up, packages and database.js

---
+++ config/database.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const mongoURI = process.env.MONGO_URI
+
+//connect to mongo
+mongoose.connect(mongoURI);
+
+//create messages to check mongo connection
+
+db.on('error', (err) => console.log(err.message + 'error with mongo connection'))
+db.on('connected', () => console.log('mongo is connected'))
+db.on('disconnected', () => console.log('mongo disconnected'))
