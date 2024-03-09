dd6a9e15c51235d203a31634d4e948e2ea6820c4
linguosity <brandon@linguosity.ai>
2024-03-09 10:00:45 -0800

    remove tooltips for syllable slider

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+/*var slider =document.getElementByID("syllSlider");
+var output = document.getElementById("addSyllables");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}*/
